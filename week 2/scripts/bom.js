0b98f9d9435bcc92dc1f382d0079c4f66a8859b9
redpants9 <redpants97@gmailcom>
2024-09-27 15:43:45 -0600

    hello

--- week 2/scripts/bom.js
+++ week 2/scripts/bom.js
@@ -2,4 +2,8 @@ const input =document.querySelector('input')
 const button =document.querySelector('button')
 const list =document.querySelector('list')
 const li =document.createElement('li')
-const deleat=document.createElement('button')
+const deleteButton=document.createElement('button')
+li.textContent = input.value
+deleteButton.textContent ='';
+li.append(deleteButton)
+list.append(li);
